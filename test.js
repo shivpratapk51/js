795ea37743f0671cf0e81ead600dfc81c7412913
Shivpratp <shivpratap05oct2007@gmail.com>
2024-06-26 20:21:28 -0700

    which one let or var

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 // multiple console log in tabular form
 const accountId = 12345
 let accoundEmail = "shivpratapk51@gmail.com"
-var accountPassword = "909090"
+var accountPassword = "909090" // we can not use var because of issues in block scope and functional scope
 accountcity = 'Kanpur'
 
 console.table([accountId, accoundEmail, accountPassword, accountcity])
