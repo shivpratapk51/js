a7ea32be7f3535ee50c1760c2dc90fc6e68bde96
Shivpratp <shivpratap05oct2007@gmail.com>
2024-06-26 20:18:38 -0700

    multiple console log in tabular form

---
+++ test.js
@@ -0,0 +1,7 @@
+// multiple console log in tabular form
+const accountId = 12345
+let accoundEmail = "shivpratapk51@gmail.com"
+var accountPassword = "909090"
+accountcity = 'Kanpur'
+
+console.table([accountId, accoundEmail, accountPassword, accountcity])
