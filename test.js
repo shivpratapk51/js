1cf2bea2b25ce4833e4b45b13fd06a53368e46df
Shivpratp <shivpratap05oct2007@gmail.com>
2024-06-26 20:23:44 -0700

    undefined variable

--- test.js
+++ test.js
@@ -3,5 +3,7 @@ const accountId = 12345
 let accoundEmail = "shivpratapk51@gmail.com"
 var accountPassword = "909090" // we can not use var because of issues in block scope and functional scope
 accountcity = 'Kanpur'
+let accountState; // undefined variable
 
-console.table([accountId, accoundEmail, accountPassword, accountcity])
+
+console.table([accountId, accoundEmail, accountPassword, accountcity, accountState])
