f2ac909ffcaa464fcbfc9231585f573fb2fffc19
Shivpratap <168255666+shivpratapk51@users.noreply.github.com>
2024-12-09 09:42:05 +0530

    Delete lecture_04.js

--- lecture_04.js
+++
@@ -1,9 +0,0 @@
-// multiple console log in tabular form
-const accountId = 12345
-let accoundEmail = "shivpratapk51@gmail.com"
-var accountPassword = "909090" // we can not use var because of issues in block scope and functional scope
-accountcity = 'Kanpur'
-let accountState; // undefined variable
-
-
-console.table([accountId, accoundEmail, accountPassword, accountcity, accountState])
